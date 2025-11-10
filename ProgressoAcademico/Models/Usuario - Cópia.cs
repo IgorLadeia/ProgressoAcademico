114c782d3feeacee8fb3470de90bfb880a3e5b28
IgorLadeia <igorladeia10@Hotmail.com>
2025-11-09 23:28:45 -0300

    terceiro commit

---
+++ ProgressoAcademico/Models/Usuario - Cópia.cs
@@ -0,0 +1,13 @@
+﻿namespace ProgressoAcademico.Models
+{
+    public class PerfilUsuario
+    {
+        public int UsuarioId { get; set; } 
+        public string? UsuarioApelido { get; set; } 
+        public string? UsuarioNome { get; set; }
+        public string? UsuarioSobreNome { get; set; }
+        public string? UsuarioTelephone { get; set; }    
+        public DateOnly UsuarioDataNascimento { get; set; }
+        
+    }
+}
---
+++ ProgressoAcademico/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace ProgressoAcademico.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string? UsuarioNome { get; set; }
+        public string? UsuarioEmail { get; set; }   
+        public string? UsuarioSenha { get; set; }
+
+        public DateOnly UsuarioDataCadastro { get; set; }  
+         
+        public Boolean UsuarioAtivo { get; set; }   
+
+
+
+    }
+
+}
