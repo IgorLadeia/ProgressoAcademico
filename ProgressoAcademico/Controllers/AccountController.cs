3b1e842e1710302723fb1f5ebe95231d8e8e889c
IgorLadeia <igorladeia10@Hotmail.com>
2026-01-12 22:45:23 -0300

    fisrt commit tela inicial

---
+++ ProgressoAcademico/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProgressoAcademico.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            //  depois você liga com autenticação real
+            if (email == "admin@ufabc.edu.br" && senha == "123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Erro = "Usuário ou senha inválidos";
+            return View();
+        }
+    }
+}
--- ProgressoAcademico/Program.cs
+++ ProgressoAcademico/Program.cs
@@ -3,41 +3,44 @@ using ProgressoAcademico.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Éssa parte é muito importante é aqui que estou definindo todos os serviços que a aplicação vai usar.
-// perceba que o builder.Services é uma coleção de serviços que a aplicação vai utilizar.
-// existem muitos serviços que podem ser adicionados aqui, dependendo das necessidades da aplicação.
-// o entyti framework, autenticação, autorização, logging, etc.
-// todos serão servições que a aplicação pode usar.
-
+// =====================
+// Services
+// =====================
 
 var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<ProgressoAcademicoDbContext>(options =>
+    options.UseMySql(
+        mySqlConnection,
+        ServerVersion.AutoDetect(mySqlConnection)
+    ));
 
-builder.Services.AddDbContext<ProgressoAcademicoDbContext>(options=>options.UseMySql(mySqlConnection,ServerVersion.AutoDetect(mySqlConnection)));
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddControllers();
-// o builder.Services.AddDbContext é usado para adicionar o contexto do banco de dados à aplicação.
-// nesse caso, estou usando o contexto ProgressoAcademicoContext que está definido na pasta Data.
-// o options.UseSqlServer é usado para configurar a conexão com o banco de dados MySQL Server.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
 
+// =====================
+// App
+// =====================
 
 var app = builder.Build();
 
-// configura o pipeline de requisições HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// app.UseHttpsRedirection é usado para redirecionar todas as requisições HTTP para HTTPS.
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
+
 app.UseHttpsRedirection();
-// app.UseAuthorization é usado para habilitar a autorização na aplicação.
+
 app.UseAuthorization();
-// app.MapControllers é usado para mapear os controladores da aplicação.
+
 app.MapControllers();
-// por fim, o app.Run é usado para iniciar a aplicação.
+
 app.Run();
